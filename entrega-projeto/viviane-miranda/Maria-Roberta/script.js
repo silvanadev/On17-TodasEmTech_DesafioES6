7e37058c39c152338441ccb4ef0676423c347bd3
Roberta Gomes <roberta.cbrecife@gmail.com>
2022-06-19 16:52:44 -0300

    inicio do desafio

---
+++ entrega-projeto/viviane-miranda/Maria-Roberta/script.js
@@ -0,0 +1,54 @@
+const input = document.querySelector(".input");
+const button = document.querySelector(".button-submit");
+const divCard = document.querySelector(".card-principal");
+
+
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+    const userName = input.value.trim();
+    if (userName) {
+        getGitHubData(userName);
+    } else {
+        alert('Digite algum usuário!');
+    }
+    input.value = ''; //limpar o input depois de executar a pesquisa
+});
+
+
+const getGitHubData = async (user) => {
+    const url = `https://api.github.com/users/${user}`;
+    try {
+        const response = await fetch(url);
+        if(response.ok){
+            const data = await response.json();
+            divCard.innerHTML = createCard(data);
+        } else{
+            alert("esta usuária não existe no github!");
+            throw new Error();
+        }
+    }
+    catch (err) {
+        console.error("a requisição não foi bem-sucedida", err);
+    }
+};
+
+function createCard(user){
+    const { name, avatar_url, bio, public_repos, login, followers } = user;
+    return `
+    <img class="img-github" src="${avatar_url}" alt="minha foto do github" />
+    <h2 class="name" >name: ${name}</h2>
+    <h3 class="login" >login: ${login}</h3>
+    <p class="bio" >bio: ${bio ? bio : ''}</p>
+
+    <div class="caixa-iconne-numeros">
+        <img class="icone1" src="../../../images/people_outline.png"/>
+        <p class="public_repos" >public repos: ${public_repos}</p>
+    </div>
+
+    <div class="caixa-iconne-numeros"> 
+        <img class="icone2" src="../../../images/Vector.png"/>
+        <p class="followers"${followers}</p> 
+    </div>
+    
+    `
+};
